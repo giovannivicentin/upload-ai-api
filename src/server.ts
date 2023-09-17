d116aa28e051c497cfcfb2cd7bd5cf0145aeb268
Giovanni Vicentin <giovannifvicentin@gmail.com>
2023-09-17 00:46:40 -0300

    create a method to do the transcription with OpenAI API

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import { fastify } from "fastify";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { UploadVideoRoute } from "./routes/upload-video";
+import { createTranscriptionRoute } from "./routes/create-transcription";
+
 
 const app = fastify();
 
 app.register(getAllPromptsRoute);
 app.register(UploadVideoRoute);
+app.register(createTranscriptionRoute)
 
 app
   .listen({
